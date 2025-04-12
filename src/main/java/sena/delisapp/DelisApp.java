6462660c48d34da59631b29017da3c349e4eff0d
pauchacon16 <pauchacon16@gmail.com>
2025-04-12 16:48:40 -0500

    Iniciar Proyecto DelisApp

---
+++ src/main/java/sena/delisapp/DelisApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sena.delisapp;
+
+/**
+ *
+ * @author jseba
+ */
+public class DelisApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
